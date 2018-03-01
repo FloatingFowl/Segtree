5c63b840a869621239d4d487d5bfa0317b799ff2
Mohsin Mustafa <mohsin.mustafa@students.iiit.ac.in>
2018-03-01 11:12:18 +0530

    Recursive query and update added.

--- main.cpp
+++ main.cpp
@@ -7,5 +7,9 @@ int main(){
     //For Testing Purposes
     vector<int> temp = {1,2,3,4};
     SegmentTree<int> st{temp, [](int& fi, int& se){return fi + se;}};
-    cout<<SegmentTree<int>::GetTreeSize(5)<<'\n';
+    cout<<st.Query(0,2)<<'\n';
+    st.Update(5,1);
+    st.DebugPrint();
+    cout<<st.Query(0,3)<<'\n';
+    //cout<<st.Query(0,4)<<'\n';
 }
--- segtree.cpp
+++ segtree.cpp
@@ -3,8 +3,8 @@
 #ifndef _SEGMENTTREE_CPP_
 #define _SEGMENTTREE_CPP_
 
-template <typename base>
-SegmentTree<base>::SegmentTree(std::vector<base> const &init_values, std::function<base(base&, base&)> bin_func)
+template <typename Base>
+SegmentTree<Base>::SegmentTree(std::vector<Base> const &init_values, std::function<Base(Base&, Base&)> bin_func)
     : bin_func_(bin_func)
     , len_(init_values.size())
 {
@@ -13,8 +13,9 @@ SegmentTree<base>::SegmentTree(std::vector<base> const &init_values, std::functi
     DebugPrint();
 }
 
-template <typename base>
-void SegmentTree<base>::BuildTree(std::size_t l_index, std::size_t r_index, std::vector<base> const &init_values, std::size_t tree_index)
+
+template <typename Base>
+void SegmentTree<Base>::BuildTree(std::size_t l_index, std::size_t r_index, std::vector<Base> const &init_values, std::size_t tree_index)
 {
     if (l_index == r_index)
     {
@@ -31,20 +32,98 @@ void SegmentTree<base>::BuildTree(std::size_t l_index, std::size_t r_index, std:
 }
 
 
-template <typename base>
-size_t SegmentTree<base>::GetTreeSize(size_t const &len)
+template <typename Base>
+Base SegmentTree<Base>::QueryRecursive(std::size_t l_qbound, std::size_t r_qbound, std::size_t l_index, std::size_t r_index, std::size_t tree_index)
+{
+    if (l_qbound <= l_index && r_index <= r_qbound)
+    {
+        return tree_[tree_index];
+    }
+
+    std::size_t boundary = (l_index + r_index) >> 1;
+    std::size_t next_tree_index = (tree_index << 1) + 1;
+
+    if (r_qbound <= boundary)
+    {
+        return QueryRecursive(l_qbound, r_qbound, l_index, boundary, next_tree_index);
+    }
+    else if (l_qbound > boundary)
+    {
+        return QueryRecursive(l_qbound, r_qbound, boundary + 1, r_index, next_tree_index + 1);
+    }
+    else
+    {
+        Base l_query = QueryRecursive(l_qbound, boundary, l_index, boundary, next_tree_index);
+        Base r_query = QueryRecursive(boundary + 1, r_qbound, boundary + 1, r_index, next_tree_index + 1);
+        return bin_func_(l_query, r_query);
+    }
+}
+
+
+template <typename Base>
+void SegmentTree<Base>::UpdateRecursive(Base const &new_value, std::size_t const &final_index, std::size_t l_index, std::size_t r_index, std::size_t tree_index)
+{
+    if (l_index == r_index)
+    {
+        tree_[tree_index] = new_value;
+        return;
+    }
+
+    std::size_t boundary = (l_index + r_index) >> 1;
+    std::size_t next_tree_index = (tree_index << 1) + 1;
+
+    if (final_index <= boundary)
+    {
+        UpdateRecursive(new_value, final_index, l_index, boundary, next_tree_index);
+    }
+    else
+    {
+        UpdateRecursive(new_value, final_index, boundary + 1, r_index, next_tree_index + 1);
+    }
+    tree_[tree_index] = bin_func_(tree_[next_tree_index], tree_[next_tree_index + 1]);
+}
+
+
+template <typename Base>
+Base SegmentTree<Base>::Query(std::size_t l_qbound, std::size_t r_qbound)
+{
+    if (l_qbound < 0 || l_qbound >= len_ || r_qbound < 0 || r_qbound >= len_)
+    {
+        throw std::out_of_range("The indices must be within the range of the segment tree.");
+    }
+    if (l_qbound > r_qbound)
+    {
+        throw std::out_of_range("The left index must be smaller than the right index.");
+    }
+    return QueryRecursive(l_qbound, r_qbound, 0, len_ - 1, 0);
+}
+
+template <typename Base>
+void SegmentTree<Base>::Update(Base const &new_value, std::size_t const &index)
+{
+    if (index < 0 || index > len_)
+    {
+        throw std::out_of_range("The index must be within the range of the segment tree.");
+    }
+    UpdateRecursive(new_value, index, 0, len_ - 1, 0);
+}
+
+
+template <typename Base>
+size_t SegmentTree<Base>::GetTreeSize(size_t const &len)
 {
     return 2 * std::pow(2, std::ceil(std::log2(len))) - 1;
 }
 
 
-template <typename base>
-void SegmentTree<base>::DebugPrint()
+template <typename Base>
+void SegmentTree<Base>::DebugPrint()
 {
     int limit = GetTreeSize(len_);
     for (int i = 0; i < limit; i++){
         std::cout<<tree_[i]<<"\t";
     }
+    std::cout<<'\n';
 }
 
 #endif
